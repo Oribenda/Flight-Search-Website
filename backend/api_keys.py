0acb257e05543ea9acaa2d8c84ecc6185e11e7d8
Ori Ben David <ori8000@gmail.com>
2023-11-03 17:24:00 +0200

    fix api_key error and push to docker automation

--- backend/api_keys.py
+++ backend/api_keys.py
@@ -1,12 +1,20 @@
 import os
 
-open_ai_key = os.environ["OPEN_AI_KEY"]
+def get_api_key(env_var_name):
+    api_key = os.environ.get(env_var_name)
+    if api_key is None:
+        api_key = ""
+    return api_key
 
-mongo_db_key = os.environ["MONGO_DB_KEY"]
+open_ai_key = get_api_key("OPEN_AI_KEY")
+mongo_db_key = get_api_key("MONGO_DB_KEY")
+amadeus_api_key = get_api_key("AMADEUS_API_KEY")
 
-amadeus_api_key = os.environ["AMADEUS_API_KEY"]
+# open_ai_key = os.environ["OPEN_AI_KEY"]
 
+# mongo_db_key = os.environ["MONGO_DB_KEY"]
 
+# amadeus_api_key = os.environ["AMADEUS_API_KEY"]
 
 
 
